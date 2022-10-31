c76479d2a056a76637b5ad7a598b2774010d874f
Emran <emran.bm@gmail.com>
2022-10-31 22:33:40 +0330

    Feat: List changed files by git repo manager

--- src/gitrepo/manager_test.go
+++ src/gitrepo/manager_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/suite"
 	"os"
 	"os/exec"
+	"path"
 	"semangit/src/utils"
 	"strings"
 	"testing"
@@ -37,8 +38,12 @@ func (s *TestSuite) SetupTest() {
 
 func (s *TestSuite) runGitCommand(args ...string) string {
 	newArgs := append([]string{"-C", s.repoDir}, args...)
-	cmd := exec.Command("git", newArgs...)
-	output := utils.GetResultOrPanicError(cmd.Output())
+	return s.runCommand("git", newArgs...)
+}
+
+func (s *TestSuite) runCommand(name string, args ...string) string {
+	cmd := exec.Command(name, args...)
+	output := utils.GetResultOrPanicError(cmd.CombinedOutput())
 	return string(output)
 }
 
@@ -56,4 +61,42 @@ func (s *TestSuite) TestCanCheckoutToAnotherBranch() {
 	s.assertBranch(MasterBranch)
 	s.repoManager.Checkout(AnotherBranch)
 	s.assertBranch(AnotherBranch)
+}
+
+func (s *TestSuite) TestCanListChangedFiles() {
+	fromRev := s.runGitCommand("log", "-1", "--pretty=%H")
+	fromRev = strings.TrimSpace(fromRev)
+	s.NotEmpty(fromRev)
+
+	s.runCommand("touch", path.Join(s.repoDir, "newfile.txt"))
+	s.runGitCommand("add", ".")
+	s.runGitCommand("commit", "-m", "Add new file")
+
+	toRev := s.runGitCommand("log", "-1", "--pretty=%H")
+	toRev = strings.TrimSpace(toRev)
+	s.NotEmpty(toRev)
+	changedFiles := s.repoManager.ListChangedFiles(fromRev, toRev)
+	s.Equal([]string{"newfile.txt"}, changedFiles)
+}
+
+func (s *TestSuite) TestChangedFilesIncludeBothOldAndNewFilenamesOnRename() {
+	s.runCommand("touch", path.Join(s.repoDir, "file1.txt"))
+	s.runGitCommand("add", ".")
+	s.runGitCommand("commit", "-m", "Add file1")
+
+	fromRev := s.runGitCommand("log", "-1", "--pretty=%H")
+	fromRev = strings.TrimSpace(fromRev)
+	s.NotEmpty(fromRev)
+
+	s.runCommand("mv", path.Join(s.repoDir, "file1.txt"), path.Join(s.repoDir, "file2.txt"))
+	s.runGitCommand("add", ".")
+	s.runGitCommand("commit", "-m", "Rename file1 to file2")
+
+	toRev := s.runGitCommand("log", "-1", "--pretty=%H")
+	toRev = strings.TrimSpace(toRev)
+	s.NotEmpty(toRev)
+	changedFiles := s.repoManager.ListChangedFiles(fromRev, toRev)
+	changedFilesStr := strings.Join(changedFiles, " ")
+	s.True(strings.Contains(changedFilesStr, "file1.txt"))
+	s.True(strings.Contains(changedFilesStr, "file2.txt"))
 }
