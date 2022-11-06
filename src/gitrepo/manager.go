7be6b55c25ef9e60b4b274e4e5943a32ad6b01b0
Emran BatmanGhelich <emran.bm@gmail.com>
2022-11-06 03:02:56 -0800

    Feat: List changed files by git repo manager (#2)
    * Feat: List changed files by git repo manager
    
    * Add doc to GetChangedFiles
    
    * Check nil for toFile

--- src/gitrepo/manager.go
+++ src/gitrepo/manager.go
@@ -9,18 +9,47 @@ import (
 const RevisionNone = ""
 
 type gitRepoManager struct {
-	worktree *git.Worktree
+	repo *git.Repository
 }
 
 func NewGitRepoManger(repoDir string) gitRepoManager {
 	repo := utils.GetResultOrPanicError(git.PlainOpen(repoDir))
-	worktree := utils.GetResultOrPanicError(repo.Worktree())
-	return gitRepoManager{worktree}
+	return gitRepoManager{repo}
 }
 
 func (m *gitRepoManager) Checkout(refName string) {
-	utils.PanicError(m.worktree.Checkout(&git.CheckoutOptions{
+	worktree := utils.GetResultOrPanicError(m.repo.Worktree())
+	utils.PanicError(worktree.Checkout(&git.CheckoutOptions{
 		Branch: plumbing.ReferenceName("refs/heads/" + refName),
 		Force:  true,
 	}))
+}
+
+// ListChangedFiles Returns the list of filenames that are changed between the two given git revisions.
+func (m *gitRepoManager) ListChangedFiles(fromRevision string, toRevision string) []string {
+	fromHash := utils.GetResultOrPanicError(m.repo.ResolveRevision(plumbing.Revision(fromRevision)))
+	fromCommit := utils.GetResultOrPanicError(m.repo.CommitObject(*fromHash))
+	toHash := utils.GetResultOrPanicError(m.repo.ResolveRevision(plumbing.Revision(toRevision)))
+	toCommit := utils.GetResultOrPanicError(m.repo.CommitObject(*toHash))
+	patch := utils.GetResultOrPanicError(fromCommit.Patch(toCommit))
+	filePathsSet := make(map[string]bool)
+	for _, filePatch := range patch.FilePatches() {
+		fromFile, toFile := filePatch.Files()
+		if fromFile != nil {
+			filePathsSet[fromFile.Path()] = true
+		}
+		if toFile != nil {
+			filePathsSet[toFile.Path()] = true
+		}
+	}
+
+	changedFilePaths := make([]string, len(filePathsSet))
+
+	i := 0
+	for path := range filePathsSet {
+		changedFilePaths[i] = path
+		i++
+	}
+
+	return changedFilePaths
 }
