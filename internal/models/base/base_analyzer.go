08e31edf7d017395da1e68f14f0bc8c52d369a27
adibov <hesamadibi80@gmail.com>
2022-11-08 17:49:16 +0330

    fix compare semantic function

--- internal/models/base/base_analyzer.go
+++ internal/models/base/base_analyzer.go
@@ -8,6 +8,10 @@ import (
 type BaseAnalyzer struct {
 }
 
+func New() *BaseAnalyzer {
+	return &BaseAnalyzer{}
+}
+
 // GetName Returns the name of the analyzer. This is the name that can be used in commandline to choose this analyzer.
 func (a *BaseAnalyzer) GetName() string {
 	panic("not implemented")
@@ -27,5 +31,11 @@ func (a *BaseAnalyzer) GetExtraArgumentDefinitions() []models.ArgumentDefinition
 
 // CompareVersions Returns 0 if the two version are equal, negative if left < right, and positive if left > right.
 func (a *BaseAnalyzer) CompareVersions(left string, right string) int {
+	if left[0] != 'v' {
+		left = "v" + left
+	}
+	if right[0] != 'v' {
+		right = "v" + right
+	}
 	return semver.Compare(left, right)
 }
