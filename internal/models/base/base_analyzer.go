58c8b8376bf0ca5058af8a6fbb36ee271102c08b
adibov <hesamadibi80@gmail.com>
2022-11-08 17:25:31 +0330

    Revert "fix compare version function"
    This reverts commit 7b58b7e0f5457ee2671288bc10c50c10537d2382.

--- internal/models/base/base_analyzer.go
+++ internal/models/base/base_analyzer.go
@@ -27,11 +27,5 @@ func (a *BaseAnalyzer) GetExtraArgumentDefinitions() []models.ArgumentDefinition
 
 // CompareVersions Returns 0 if the two version are equal, negative if left < right, and positive if left > right.
 func (a *BaseAnalyzer) CompareVersions(left string, right string) int {
-	if left[0] != 'v' {
-		left = "v" + left
-	}
-	if right[0] != 'v' {
-		right = "v" + right
-	}
-	return semver.Compare(left, right)
+	return semver.Compare("v"+left, "v"+right)
 }
