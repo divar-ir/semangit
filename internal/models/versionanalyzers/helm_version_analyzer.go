57a58c62d0c3ed37aed45a2ab6e9c9dc56b5d25d
adibov <hesamadibi80@gmail.com>
2023-01-01 18:35:09 +0330

    add debugging print for change file paths

--- internal/models/versionanalyzers/helm_version_analyzer.go
+++ internal/models/versionanalyzers/helm_version_analyzer.go
@@ -1,6 +1,7 @@
 package versionanalyzers
 
 import (
+	"github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v3"
 	"os"
 	"path/filepath"
@@ -43,7 +44,10 @@ func (a *HelmVersionAnalyzer) ChangeNeedsVersionUpdate(changedFilesPaths []strin
 	helmTemplatesRootDir := filepath.Join(helmRootDir, "templates")
 	valuesPath := filepath.Join(helmRootDir, "values.yaml")
 	for _, path := range changedFilesPaths {
+		logrus.Debug("Helm Templates Root Dir: ", helmTemplatesRootDir)
+		logrus.Debug("Values Path: ", valuesPath)
 		absPath := utils.GetResultOrPanic(filepath.Abs(path))
+		logrus.Debug("Abs Path: ", absPath)
 		if strings.HasPrefix(absPath, helmTemplatesRootDir) || absPath == valuesPath {
 			return true
 		}
